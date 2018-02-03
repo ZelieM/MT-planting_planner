29f197c79641931c354738aa22819633acc00017
ZelieM <zelie.mulders@student.uclouvain.be>
2018-02-03 13:28:29 +0100

    login system

--- planner/urls.py
+++ planner/urls.py
@@ -1,10 +1,19 @@
+from django.conf.urls import url
 from django.urls import path
+
+
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 app_name = 'planner'
 urlpatterns = [
     path('', views.index, name='index'),
+
+    url(r'^login/$', auth_views.login, {'template_name': 'planner/login.html'}, name='login'),
+
+    path('signup', views.signup, name='signup'),
+    path('gardenselection', views.gardenselection, name='gardenselection'),
     path('create_garden', views.create_garden, name='create_garden'),
     path('join_garden', views.join_garden, name='join_garden'),
     path('<int:gardenid>/add_bed', views.add_bed, name='add_bed'),
--- planner/views.py
+++ planner/views.py
@@ -1,13 +1,42 @@
+from django.db import IntegrityError
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-from .models import Garden, Bed, Vegetable, Event
+from django.contrib.auth.models import User
 
+from planner.forms import UserForm
+from .models import Garden, Bed, Vegetable, Event
+from django.contrib import messages
 from .ganttchart import create_gantt
 
-def index(request):
-    return render(request, 'planner/index.html')
+
+def index(request): ## TODO : if user logged in, redirect to gardenselection
+    return HttpResponseRedirect('login')
+
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        mail = request.POST['mailaddress']
+
+        try:
+            new_user = User.objects.create_user(username=username, password=password, email=mail)
+
+            return HttpResponseRedirect('gardenselection')
+        except IntegrityError:
+            messages.error(request, 'Le nom d\'utilisateur existe deja')
+            return render(request, 'planner/signup.html')
+
+    else:
+        return render(request, 'planner/signup.html')
+
+
+
+def gardenselection(request):
+    return render(request, 'planner/gardenselection.html')
 
 
 def create_garden(request):
@@ -46,7 +75,6 @@ def planification_view(request, garden_id):
     garden = get_object_or_404(Garden, pk=garden_id)
     beds = Bed.objects.filter(garden_id=garden.id)
     beds_ids = Bed.objects.filter(garden_id=garden.id).values_list('id', flat=True)
-    print(beds_ids)
     vegetables = Vegetable.objects.all()
     events = Event.objects.filter(bed_id__in=beds_ids)
     create_gantt(events, garden_id)
