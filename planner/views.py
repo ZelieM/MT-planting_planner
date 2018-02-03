0e0481c4c9d2844411add07b504a976ff442421f
ZelieM <zelie.mulders@student.uclouvain.be>
2018-02-03 13:39:33 +0100

    redirect if not logged in

--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse,HttpResponseRedirect
@@ -35,6 +36,7 @@ def garden_selection(request):
     return render(request, 'planner/gardenselection.html')
 
 
+@login_required(login_url="/planner/login/")
 def create_garden(request):
     new_garden_name = request.POST['gardenname']
     garden = Garden.objects.create(name=new_garden_name)
@@ -42,18 +44,21 @@ def create_garden(request):
     return HttpResponseRedirect(nextpage)
 
 
+@login_required(login_url="/planner/login/")
 def garden_view(request, garden_id):
     garden = get_object_or_404(Garden, pk=garden_id)
     beds = Bed.objects.filter(garden_id=garden.id)
     return render(request, 'planner/details.html', {'garden': garden, 'beds': beds})
 
 
+@login_required(login_url="/planner/login/")
 def join_garden(request):
     garden = get_object_or_404(Garden, name=request.POST['gardenname'])
     nextpage = garden.get_absolute_url()
     return HttpResponseRedirect(nextpage)
 
 
+@login_required(login_url="/planner/login/")
 def add_bed(request, gardenid):
     bname = request.POST['bedname']
     blength = request.POST['bedlength']
@@ -62,11 +67,13 @@ def add_bed(request, gardenid):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required(login_url="/planner/login/")
 def delete_bed(request, bedid):
     Bed.objects.get(pk=bedid).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required(login_url="/planner/login/")
 def planification_view(request, garden_id):
     garden = get_object_or_404(Garden, pk=garden_id)
     beds = Bed.objects.filter(garden_id=garden.id)
@@ -77,6 +84,7 @@ def planification_view(request, garden_id):
     return render(request, 'planner/planification.html', {'garden': garden, 'beds': beds, 'vegetables': vegetables, 'events': events})
 
 
+@login_required(login_url="/planner/login/")
 def add_event(request):
     vid = request.POST["vegetablename"]
     bid = request.POST["bedname"]
