c5bbe625eac7cc1d0d7b0ee5ae3fdca69ef57e76
ZelieM <zelie.mulders@student.uclouvain.be>
2018-03-31 19:05:46 +0200

    - remove "planner" from urls

--- planting_planner/urls.py
+++ planting_planner/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('planner/', include('planner.urls')),
+    path('', include('planner.urls')),
     path('vegetables_library', include('vegetables_library.urls')),
     path('research', include('research.urls')),
     path('admin/doc/', include('django.contrib.admindocs.urls')),
