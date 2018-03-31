fca454cfcb96beba51bb86019c9ef74558c7e269
ZelieM <zelie.mulders@student.uclouvain.be>
2018-03-31 18:41:46 +0200

    - add json file reading for secret key in wsgi

--- planting_planner/wsgi.py
+++ planting_planner/wsgi.py
@@ -14,3 +14,20 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "planting_planner.settings.produ
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
+
+
+import json
+from django.core.exceptions import ImproperlyConfigured
+
+key = "planting_planner/settings/secrets/secrets.json"
+with open(key) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secret=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
