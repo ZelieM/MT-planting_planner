9d888714ea874ba789218f25be114badd39617ad
ZelieM <zelie.mulders@student.uclouvain.be>
2018-03-31 17:47:09 +0200

    - change order

--- planting_planner/wsgi.py
+++ planting_planner/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "planting_planner.settings.production")
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "planting_planner.settings.production")
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
