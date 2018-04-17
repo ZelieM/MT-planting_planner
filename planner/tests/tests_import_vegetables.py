5085e540932ae0161db4a0188b59ad563f400c1b
ZelieM <zelie.mulders@student.uclouvain.be>
2018-04-17 21:11:24 +0200

    - test import vegetables with operations

--- planner/tests/tests_import_vegetables.py
+++ planner/tests/tests_import_vegetables.py
@@ -3,7 +3,7 @@ from datetime import date
 from django.test import TestCase
 
 from planner.import_vegetables_helpers import import_vegetables_to_garden
-from planner.models import Garden, Vegetable
+from planner.models import Garden, Vegetable, CulturalOperation
 
 OP1_NAME = "DateOperation"
 V1_NAME = "LibraryVeggie"
@@ -13,8 +13,20 @@ class ImportVegetableTests(TestCase):
 
     def setUp(self):
         self.garden = Garden.objects.create(name="MyGarden", postal_code=1000)
+
+
+    def test_import_simple_veggie(self):
         from vegetables_library.models import Vegetable as library_vegetable
         self.library_veggie = library_vegetable.objects.create(name=V1_NAME, variety="MyVariety")
+        self.assertEqual(len(self.garden.vegetable_set.all()), 0)  # Currently no vegetables in this garden
+        vegetable = [self.library_veggie.id]
+        import_vegetables_to_garden(self.garden.id, vegetable)
+        self.assertEqual(len(self.garden.vegetable_set.all()), 1)
+        self.assertEqual(Vegetable.objects.get(name=V1_NAME).extern_id,
+                         self.library_veggie.id)  # Check if the id of the original vegetable has been copied properly
+
+    def test_import_vegetable_with_operations(self):
+        from vegetables_library.models import Vegetable as library_vegetable
         self.second_veggie = library_vegetable.objects.create(name="SecondVeggie", variety="SameVariety")
         from vegetables_library.models import COWithOffset as library_co_with_offset
         from vegetables_library.models import COWithDate as library_co_with_date
@@ -24,11 +36,8 @@ class ImportVegetableTests(TestCase):
                                                     previous_operation=op1, offset_in_days=3)
         library_co_with_offset.objects.create(name="SecondOffsetOp", vegetable=self.second_veggie,
                                               previous_operation=op2, offset_in_days=3)
-
-    def test_import_simple_veggie(self):
-        self.assertEqual(len(self.garden.vegetable_set.all()), 0)  # Currently no vegetables in this garden
-        vegetable = [self.library_veggie.id]
+        vegetable = [self.second_veggie.id]
         import_vegetables_to_garden(self.garden.id, vegetable)
-        self.assertEqual(len(self.garden.vegetable_set.all()), 1)
-        self.assertEqual(Vegetable.objects.get(name=V1_NAME).extern_id,
-                         self.library_veggie.id)  # Check if the id of the original vegetable has been copied properly
+        imported_veggie = Vegetable.objects.get(name=self.second_veggie.name)
+        self.assertEqual(imported_veggie.extern_id, self.second_veggie.id)
+        self.assertEqual(len(CulturalOperation.objects.select_subclasses().filter(vegetable=imported_veggie)), 3)
