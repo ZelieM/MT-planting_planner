24ca585bcb182a17edee631740083cf21d2cf80b
ZelieM <zelie.mulders@student.uclouvain.be>
2018-04-18 10:59:05 +0200

    - test_bed_management

--- planner/custom_decorators.py
+++ planner/custom_decorators.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
 
 def custom_login_required(function=None, login_url="/login/"):
     """
-    Custome decorator with a login url set based on this application
+    Custom decorator with a login url set based on this application
     """
     return login_required(function, login_url=login_url)
+
--- planner/tests/tests_bed_management_views.py
+++ planner/tests/tests_bed_management_views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase, Client
 
-from planner.models import Garden
+from planner.models import Garden, Bed
 
 
 class BedManagementViewsTests(TestCase):
@@ -32,3 +32,25 @@ class BedManagementViewsTests(TestCase):
         self.assertRedirects(response, expected_url='/{}/'.format(self.garden.id), status_code=302,
                              target_status_code=200)
         self.assertEqual(len(self.garden.bed_set.all()), 1)
+
+    def test_update_bed(self):
+        bed = Bed.objects.create(garden=self.garden, name='FirstBed', width=250, length=250)
+        login = self.client.login(username=self.username, password=self.password)
+        response = self.client.get('/{}/update_bed/{}'.format(self.garden.id, bed.id))
+        self.assertEqual(response.status_code, 200)
+        form = {'name': 'ChangingName', 'length': '450', 'width': '350', 'exposition': 'NO', 'comment': 'nocomment',
+                'soil_type': 'oily'}
+        response = self.client.post('/{}/update_bed/{}'.format(self.garden.id, bed.id), data=form)
+        self.assertRedirects(response, expected_url='/{}/'.format(self.garden.id), status_code=302,
+                             target_status_code=200)
+        self.assertEqual(Bed.objects.get(pk=bed.id).name, 'ChangingName')
+
+    def test_delete_bed(self):
+        bed = Bed.objects.create(garden=self.garden, name='FirstBed', width=250, length=250)
+        login = self.client.login(username=self.username, password=self.password)
+        response = self.client.get('/{}/delete_bed/{}'.format(self.garden.id, bed.id))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.post('/{}/delete_bed/{}'.format(self.garden.id, bed.id))
+        self.assertRedirects(response, expected_url='/{}/'.format(self.garden.id), status_code=302,
+                             target_status_code=200)
+        self.assertFalse(len(Bed.objects.filter(pk=bed.id)), 0)
