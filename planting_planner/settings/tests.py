4480d014939ad8cfa54755f23c9a48fddc61acd5
ZelieM <zelie.mulders@student.uclouvain.be>
2018-05-09 16:09:05 +0200

    - change tests settings for some test that fail because of sqlite

--- planting_planner/settings/tests.py
+++ planting_planner/settings/tests.py
@@ -1,5 +1,5 @@
 from .dev import *
 
 # Speed up tests by using sqlite engine
-DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
-DATABASES['db_vegetables_library']['ENGINE'] = 'django.db.backends.sqlite3'
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql'
+DATABASES['db_vegetables_library']['ENGINE'] = 'django.db.backends.postgresql'
